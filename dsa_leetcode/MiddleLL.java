40f72ef0aa02be0dd344c455adee41743a00894a
Anvesh Khambadkar <khambadkaranvesh@gmail.com>
2023-03-23 16:00:42 +0530

    Calculate the middle of the linked list leetcode question

---
+++ dsa_leetcode/MiddleLL.java
@@ -0,0 +1,62 @@
+package dsa_leetcode;
+
+public class MiddleLL {
+
+    private Node head;
+
+    void insert(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    private int middle(Node head){
+        Node fast = head;
+        Node slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow.data;
+    }
+
+    void display(){
+        if(head == null){
+            System.out.println("Nothing to print");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        MiddleLL list = new MiddleLL();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+        list.display();
+
+        System.out.println(list.middle(list.head));
+    }
+}
