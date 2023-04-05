00dd037f6b57b31b4da95991ee15ea4642a30110
Anvesh Khambadkar <khambadkaranvesh@gmail.com>
2023-04-05 09:27:10 +0530

    Stock Span problem

---
+++ dsa_leetcode/StockSpanProblem.java
@@ -0,0 +1,45 @@
+package dsa_leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+
+public class StockSpanProblem {
+    static Queue<Integer> stockSpan(int[] nums){
+        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> index = new Stack<>();
+        Queue<Integer> ans = new PriorityQueue<>();
+
+        for(int i = 0; i<nums.length; i++){
+            if(stack.size() == 0){
+                ans.offer(1);
+                index.push(i);
+            }
+            else if(stack.size() > 0 && stack.peek() > nums[i]){
+                ans.offer(i - index.peek());
+                index.push(i);
+            }
+            else if(stack.size() > 0 && stack.peek() <= nums[i]){
+                while(stack.size() > 0 && stack.peek() <= nums[i]){
+                    stack.pop();
+                    index.pop();
+                }
+                if(stack.size() == 0){
+                    ans.offer(i+1);
+                }
+                else{
+                    ans.offer(i-index.peek());
+                    index.push(i);
+                }
+            }
+            stack.push(nums[i]);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 80, 60, 70, 65, 75, 85};
+        System.out.println(stockSpan(nums));
+    }
+}
